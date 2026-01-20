44c9fe549090bd8d0d9640d3e38a6711f24d1496
LeonardoLeporati7 <leporati.leonardo7@gmail.com>
2026-01-20 12:48:42 +0100

    Adjust terrain generation and lighting settings
    Updated mesh_instance_3d.gd to refine terrain resolution, height, and color assignment logic for improved visual fidelity. Modified node_2d.tscn to tweak material transparency, adjust light energy, and set certain lights to invisible for better scene lighting control.

--- pistolo/mesh_instance_3d.gd
+++ pistolo/mesh_instance_3d.gd
@@ -1,8 +1,8 @@
 extends MeshInstance3D
 
 @export var size := 70.0          # dimensione isola
-@export var resolution := 324     # più basso = più low poly
-@export var height := 6         # altezza massima
+@export var resolution := 48     # più basso = più low poly
+@export var height := 7    # altezza massima
 
 @export var noise_scale := 0.1
 
@@ -38,22 +38,28 @@ func generate_terrain():
 		var dist := Vector2(v.x, v.z).length() / (size * 0.5)
 		var island_mask :float = clamp(1.0 - dist, 0.0, 1.0)
 		
+		
 		v.y = h * height * island_mask
 		vertices[i] = v
 		
-		if cosaaa < 20:
-			print("v che cazzo ne so: ", island_mask )
-			cosaaa+=1
+		if i  == 400:
+			print("v che cazzo ne so: ", island_mask)
+			
+			
+		var height_percent = (v.y + 1.0) / (height + 1.0) 
+		height_percent = clamp(height_percent, 0.0, 1.0)
 		
-		if(v.y < -0.8):
-			print(v)
-			colors[i]= Color(0.0, 0.72, 0.989, 8.0)
-		elif (v.y < 0.6):
-			colors[i]= Color(0.417, 0.692, 0.331, 1.0)
+		if(height_percent < 0.000001):
+			print(height_percent)
+			colors[i]= Color(0.0, 0.72, 0.989, 1)
+		elif (height_percent < 0.2):
+			colors[i]= Color(0.417, 0.692, 0.331, 1)
 		else:
-			colors[i]= Color(0.737, 0.737, 0.737, 1.0)
+			colors[i]= Color(0.737, 0.737, 0.737, 1)
 		
-		
+		if(island_mask==0):
+				
+				colors[i]= Color(0.417, 0.692, 0.331, 0.0)
 		
 	
 	arrays[ArrayMesh.ARRAY_VERTEX] = vertices
